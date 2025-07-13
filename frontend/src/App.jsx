2f7f6085cad9e8904f011483277fe87d15c31ce6
dannydog001 <ramanforjobs001@gmail.com>
2025-07-13 16:50:10 +0530

    ,

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,7 +21,6 @@ import CoverLetterBuilder from './component/coverletter'
 import AboutPage from './component/aboutus'
 import CareersPage from './component/careerpage'
 import PressPage from './component/presspage'
-kakaks
 import HelpCenterPage from './component/helpcentre'
 import JobAlertsPage from './component/smartjob'
 import UserDashboard from './component/userdasboard'
